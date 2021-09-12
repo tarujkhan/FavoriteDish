e10a17a41ce3f2a636450e082c84b09dc4c548eb
tarujkhan <tarujkhan@yahoo.com>
2021-09-12 16:07:21 -0500

    (Still setting up Github with the user and sessions controller. Just created the create method.)

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :dishes
   resources :cuisines
   resources :foodlovers
+  get 'auth/github/callback' => 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
