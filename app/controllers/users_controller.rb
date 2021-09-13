5ad8acf4863729da89a403c14406d337a56799dd
tarujkhan <tarujkhan@yahoo.com>
2021-09-13 11:51:54 -0500

    (Setting up my user controller with all its actions. Next will work on the view file.)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,16 @@
 class UsersController < ApplicationController
-def home
+def index
+    @users = User.all
 end
 
 def show
+    @user = User.find(params[:id])
+end
+
+def update
+end
+
+def delete
 end
 
 end 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_12_211541) do
+ActiveRecord::Schema.define(version: 2021_09_13_162516) do
 
   create_table "cuisines", force: :cascade do |t|
     t.string "name"
