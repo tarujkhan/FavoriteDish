27a68f68754c95710a6e142908eb1c5001d557d6
tarujkhan <tarujkhan@yahoo.com>
2021-09-26 16:58:25 -0500

    (There was an error in the scope part of omniauth which I just fixed. But the users/showpage still needs addition.)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,12 +16,13 @@ class SessionsController < ApplicationController
     @user = User.find_or_create_by(uid: auth['uid']) do |u|
         u.name = auth['info']['name']
         u.email = auth['info']['email']
+        u.uid = auth['info']['uid']
         end 
     
     session[:user_id] = @user.uid
-    # byebug
+    #  byebug
     redirect_to users_path
-    end
+   end
 
     def omniauth
     @user = User.from_omniauth(auth)
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
     provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    {:scope => "user.email",}
   end
+
 
   OmniAuth.config do |config|
     # Disable the redirect to /auth/failure and always raise
