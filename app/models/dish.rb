6173d1e5402bf0dfa6c10f3db10a40e68a3dbf53
tarujkhan <tarujkhan@yahoo.com>
2021-10-07 17:56:28 -0500

    (So far the database is seeded with the new cuisines. Now finishing the view page for dishes new.)

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,8 +1,8 @@
 class Dish < ApplicationRecord
     belongs_to :user
-    belongs_to :cuisine
+    belongs_to :cuisine, dependent: destroy
     accepts_nested_attributes_for :cuisine 
     accepts_nested_attributes_for :user
-    dependent_destroy: ::category
+    
     # scope :sort_a_to_z, -> {where("price > ?", 50).order(:name)}
 end
